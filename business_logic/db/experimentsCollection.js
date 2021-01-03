a952fa62f7568d648089ba80c08aaf518c9dc487
dekellevy93 <dekelle@post.bgu.ac.il>
2021-01-03 10:24:46 +0200

    finished the db function for hackathon

--- business_logic/db/experimentsCollection.js
+++ business_logic/db/experimentsCollection.js
@@ -6,22 +6,8 @@ const uri = "mongodb+srv://dekellevy:dekeldekel@twixper0.jo1eq.mongodb.net/Twixp
 const client = new MongoClient(uri,{useNewUrlParser: true, useUnifiedTopology: true});
 
 
-async function run() {
-    try {
-      await client.connect();
-  
-      const database = client.db('Twixper');
-      const collection = database.collection('Participants');
-      const query = { _id: 2, name: 'nir dz' }; 
-      const user = await collection.insertOne(query);
-  
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
-
   //delete the last experiment and insert the new one
   function insertExperiment (experiment){
-
+    collection.remove({});
+    collection.insert(experiment);
   }
