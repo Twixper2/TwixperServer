9534e7c68fa808a228fae3bb1910844ff817e3f8
nirdz <nirdz@post.bgu.ac.il>
2021-01-04 23:07:55 +0200

    DB functions signatures and helper file

--- business_logic/db/DBCommunicator.js
+++ business_logic/db/DBCommunicator.js
@@ -14,10 +14,45 @@ experimentsCollection.loadExperimentsCollection(database_global)
 actionsCollection.loadActionsCollection(database_global)
 tweetsCollection.loadTweetsCollection(database_global)
 
+function insertExperiment (experiment){
+    experimentsCollection.insertExperiment(experiment)
+    // Call for more collection modules if needed.
+}
 
 function insertParticipant(participant) {
+    // Insert to experimentsCollection
+    // Insert to participantsCollection
+}
+
+function insertAction(action){
 
 }
+
+function getParticipant(pId){
+
+}
+
+// For Hackathon, remove after it finishes.
+// Returns all the experiments in the db.
+function getExperiments(){
+
+}
+
+// For report
+function getExperiment(expId){
+
+}
+
+function getExperimentActions(expId){
+
+}
+
+// After Hackathon finishes:
+function getResearcherExperiments(researcherId){
+
+}
+
+
 
 
 
