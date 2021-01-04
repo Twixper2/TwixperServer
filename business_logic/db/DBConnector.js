bd3528338bb8b46e182116d531ec225be3e7b94a
talfrim <49164423+talfrim@users.noreply.github.com>
2021-01-04 21:46:50 +0200

    deleted unused functions

--- business_logic/db/DBConnector.js
+++ business_logic/db/DBConnector.js
@@ -3,61 +3,7 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://dekellevy:dekeldekel@twixper0.jo1eq.mongodb.net/Twixper()?retryWrites=true&w=majority";
 
 var database_global = null
-var participantsCollection_global = null
-var experimentsCollection_global = null
-var researchersCollection_global = null
-var actionsCollection_global = null
-var tweetsCollection_global = null
 
-
-
-async function getExperimentsCollection() {
-    try {
-        if (!experimentsCollection_global)
-            let database = await this.getDatabase()
-            experimentsCollection_global = await database.collection('Experiments');
-        return experimentsCollection_global
-    }
-    catch {
-        return null;
-    }
-}
-
-async function getResearchersCollection() {
-    try {
-        if (!researchersCollection_global)
-            let database = await this.getDatabase()
-            researchersCollection_global = await database.collection('Researchers');
-        return researchersCollection_global
-    }
-    catch {
-        return null;
-    }
-}
-
-async function getActionsCollection() {
-    try {
-        if (!actionsCollection_global)
-            let database = await this.getDatabase()
-            actionsCollection_global = await database.collection('Actions');
-        return actionsCollection_global
-    }
-    catch {
-        return null;
-    }
-}
-
-async function getTweetsCollection() {
-    try {
-        if (!tweetsCollection_global)
-            let database = await this.getDatabase()
-            tweetsCollection_global = await database.collection('Tweets');
-        return tweetsCollection_global
-    }
-    catch {
-        return null;
-    }
-}
 
 
 async function getDatabase() {
