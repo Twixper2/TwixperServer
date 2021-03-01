48ace1857b08a0287dbe054c41738a16492a8299
talfrim <49164423+talfrim@users.noreply.github.com>
2021-03-01 20:49:23 +0200

    started use case

--- controller/researchers/researchersAuthController.js
+++ controller/researchers/researchersAuthController.js
@@ -5,7 +5,7 @@ const client = new OAuth2Client(CLIENT_ID);
 
 /**
  * Verify the id_token, check if the researcher already in database. If so, give him cookie.
- * If not, create new researcher and  give him cookie
+ * If not, create new researcher and give him cookie
  */
 router.post("/researcherGoogleLogin", async (req, res, next) => {
     let id_token = req.body.id_token
@@ -14,9 +14,19 @@ router.post("/researcherGoogleLogin", async (req, res, next) => {
     }
     try {
         const researcherId = await verifyGoogleUser(id_token) //store this id as key in user record
+        if (!researcherId) {
+            res.sendStatus(401); // not a google user
+        }
+        let addedResearcher = await researcherService.registerResearcher(researcherId)
+        if (addedResearcher) {
+            res.sendStatus(200)
+        }
+        else {
+            res.sendStatus(500);
+        }
     }
-    catch (e) { //verify failed
-        res.sendStatus(401)
+    catch (e) { //network failed
+        res.sendStatus(500)
     }
 });
 
