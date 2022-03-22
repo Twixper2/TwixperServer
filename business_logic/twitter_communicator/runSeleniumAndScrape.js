1e6c3c4fa71b09ee55f4ce04c67510300a170167
liadna1995 <liadna1995@gmail.com>
2022-03-22 21:38:15 +0200

    fixed async functions

--- business_logic/twitter_communicator/runSeleniumAndScrape.js
+++ business_logic/twitter_communicator/runSeleniumAndScrape.js
@@ -2,41 +2,13 @@ var authorizeUser = require("./selenium_authorize/authorizeUser.js");
 var scrapeTwitter = require("./scrapy_use_auth/scrapeTwitter.js");
 var credentials = require("./static_twitter_data/CredentialsJSON.js");
 
-
-function mainRun(){
+async function mainRun(){
     // Retrieve user credentials
     var user_credentials = credentials.credentials;
     // Log in to twitter and get cookies
     var port = 5502;
-
-
-    authorizeUser.logInProcess(user_credentials)
-    .then(cookies => {
-        console.log("after first than");
-        console.log(cookies);
-        scrapeTwitter.dataTransformationToScrape(port,cookies)
-        .then(res => {
-            console.log("after second than");
-        })
-    })
-
-
-
-
-    // authorizeUser.logInProcess(user_credentials).then(function (cookies) {
-    //     setTimeout(undefined, 2);
-    //     console.log("cookies after than");
-    //     console.log(cookies)
-    //     // Next - send cookies to beautifulsoup in Python
-    //     scrapeTwitter.dataTransformationToScrape(port,cookies).then(function () {
-    //         console.log("here");
-    //     });
-    // })
-
-
-    
-
-    
+    var cookies = await authorizeUser.logInProcess(user_credentials);
+    // Next - send cookies to beautifulsoup in Python
+    await scrapeTwitter.dataTransformationToScrape(port,cookies);
 }
-
 mainRun();
--- business_logic/twitter_communicator/scrapy_use_auth/scrapeTwitter.js
+++ business_logic/twitter_communicator/scrapy_use_auth/scrapeTwitter.js
@@ -2,7 +2,7 @@ var zeromq = require("zeromq");
 var socket = zeromq.createSocket('rep');
 
 async function dataTransformationToScrape(port,selenium_cookies){
-    if( selenium_cookies != undefined){
+    if(selenium_cookies != undefined){
         socket.bind("tcp://127.0.0.1:"+port,
         function(err)
         {
@@ -12,7 +12,6 @@ async function dataTransformationToScrape(port,selenium_cookies){
             socket.on('message', function(envelope, blank, data)
                       {
                         console.log(envelope.toString('utf8'));
-                        console.log("before send cookies");
                         console.log(selenium_cookies);
                         socket.send(envelope.toString('utf8') + selenium_cookies);
                         // socket.send(envelope.toString('utf8') + " Blancmange!");
